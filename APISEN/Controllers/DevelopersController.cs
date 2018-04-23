fc87ea35e5f579a3d405bef8f3d40e5ce4d7a23c
Senura Dissanayake <delgahadeniya.dissanayake@students.plymouth.ac.uk>
2018-04-23 09:52:19 +0530

    updated the developers endpoint route and status code

--- APISEN/Controllers/DevelopersController.cs
+++ APISEN/Controllers/DevelopersController.cs
@@ -9,24 +9,72 @@ namespace ApiSenDS.Controllers
     [Route("api/[controller]")]
     public class DevelopersController: Controller
     {
-        // GET api/Developers/1
+        // GET api/Developers/ot/1
         /// <summary>
         /// Get The OverTime for a  Developer
         /// </summary>
         /// <param name="id">Developer ID</param>
         /// <returns>Json Object</returns>
         /// <response code="200">Json Object with OverTime Value</response>
+        /// <response code="204">No Content To Display</response>
         /// <response code="500">Database Is Not Online</response>
-        [HttpGet("{id}")]
-        public OTResult Get(int id)
+        [HttpGet("{ot}/{id}")]
+        public OTResult Get(int id,String ot)
         {
-            SenDBContext context = HttpContext.RequestServices.GetService(typeof(TodoApi.Models.SenDBContext)) as SenDBContext;
+            if (ot != "ot")
+            {
+                return null;//status 204
+            }
+            else
+            {
 
-            OTResult result = new OTResult() { OverTime= context.GetDeveloperOverTime(id),
-                DeveloperID =id
-            };
+                SenDBContext context = HttpContext.RequestServices.GetService(typeof(TodoApi.Models.SenDBContext)) as SenDBContext;
 
-            return result;
+                String overTimeVal = context.GetDeveloperOverTime(id);
+
+                if (overTimeVal != "")
+                {
+
+                    OTResult result = new OTResult()
+                    {
+                        OverTime = overTimeVal,
+                        DeveloperID = id
+                    };
+
+
+                    return result;
+                }
+                else{
+                    return null;//no overtime val, Thus, No response.//status 204
+                }
+
+
+            }
         }
+
+
+
+        // POST api/developers
+        /// <summary>
+        /// Create A Developer
+        /// </summary>
+        [HttpPost]
+        public IActionResult CreateANewDeveloper([FromBody]Developers newDevData)
+        {
+            if (newDevData == null)
+            {
+                return BadRequest();
+            }
+
+            SenDBContext _context = HttpContext.RequestServices.GetService(typeof(TodoApi.Models.SenDBContext)) as SenDBContext;
+
+            int status = _context.InsertNewDeveloper(newDevData.Name);
+
+
+            return CreatedAtRoute("GetDevByID", new { id = status }, newDevData);
+        }
+
+
+
     }
 }
